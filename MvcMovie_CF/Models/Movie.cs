7d1499e23c7854de84fd21509bda1fda00dc87d5
Mack <macknz7@gmail.com>
2020-10-14 14:14:53 -0700

    Part 6 complete

--- MvcMovie_CF/Models/Movie.cs
+++ MvcMovie_CF/Models/Movie.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,10 +10,16 @@ namespace MvcMovie_CF.Models
     public class Movie
     {
         public int Id { get; set; }
+
         public string Title { get; set; }
+
+        [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
+
         public string Genre { get; set; }
+
+        [Column(TypeName = "decimal(18,2)")]
         public decimal Price { get; set; }
 
     }
