59441275869ec7d29a9c6b41150c56e71f456740
Mack <macknz7@gmail.com>
2020-10-14 10:01:30 -0700

    Initial commit

---
+++ MvcMovie_CF/Controllers/HelloWorldController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace MvcMovie_CF.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string Welcome(string name, int ID = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Welcome {name}, ID: {ID}...");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# MvcMovie_CF
+ 
